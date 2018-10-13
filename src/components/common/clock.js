5d184e64fca16b67e597f59f9c6df448459648ba
killapop <killapop@protonmail.com>
2018-10-13 14:11:33 +0000

    fix clock timer style

--- src/components/common/clock.js
+++ src/components/common/clock.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './clock.css';
 
 class Clock extends React.Component {
   constructor(props) {
@@ -22,9 +23,26 @@ class Clock extends React.Component {
   startClock() {
     this.clockTimer = window.setInterval(() => {
       let c = this.state.timer;
+      const mid = c / 2;
+      let r = 0,
+        g = 0,
+        b = 0,
+        bgColor = '';
+
+      if (c >= mid) {
+        r = Math.floor(255 * (c / mid));
+        g = 255;
+      } else {
+        r = 255;
+        g = Math.floor((255 * (mid - ((c - 1) % mid))) / mid);
+      }
+      console.log(r, g, b);
+      bgColor = 'rgba(' + r + ', ' + g + ', ' + b + ', 0.8)';
+
       c--;
       this.setState(state => ({
-        timer: c
+        timer: c,
+        bgColor
       }));
     }, 1000);
   }
@@ -37,7 +55,15 @@ class Clock extends React.Component {
     const { timer } = this.state;
     return (
       <div className="clock">
-        {new Date(timer * 1000).toISOString().substr(14, 5)}
+        <div className="clock-title">time remaining</div>
+        <div className="clock-timer">
+          {new Date(timer * 1000).toISOString().substr(14, 5)}
+          <i
+            className="reset fa fa-sync pointer"
+            alt="Refresh session"
+            title="Refresh session"
+          />
+        </div>
       </div>
     );
   }
