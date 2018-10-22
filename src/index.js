f6410c31e7ed45be5a96c1e8015bbe5730371571
killapop <killapop@protonmail.com>
2018-10-22 11:02:35 +0000

    remove unneeded fetch in src/index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { fetchData } from './lib/data';
-
 ReactDOM.render(<App />, document.getElementById('root'));
-fetchData();
 registerServiceWorker();
