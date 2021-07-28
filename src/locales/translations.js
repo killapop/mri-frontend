4faf1c3440e367484493e5391e47110ab55ea929
whoohoo <killapop@protonmail.com>
2021-07-28 11:29:26 +0000

    remove es from languages

--- src/locales/translations.js
+++ src/locales/translations.js
@@ -1,7 +1,5 @@
 import { translation as en_translation } from "./en/translation.js";
 import { form as en_form } from "./en/form.js";
-import { translation as es_translation } from "./es/translation.js";
-import { form as es_form } from "./es/form.js";
 import { translation as fr_translation } from "./fr/translation.js";
 import { form as fr_form } from "./fr/form.js";
 import { translation as de_translation } from "./de/translation.js";
@@ -21,12 +19,6 @@ export const languages = [
     icon: "",
   },
   {
-    code: "es",
-    title: "Español",
-    rtl: false,
-    icon: "",
-  },
-  {
     code: "de",
     title: "Deutsch",
     rtl: false,
@@ -42,10 +34,6 @@ export const resources = {
   fr: {
     translation: fr_translation,
     form: fr_form,
-  },
-  es: {
-    translation: es_translation,
-    form: es_form,
   },
   de: {
     translation: de_translation,
